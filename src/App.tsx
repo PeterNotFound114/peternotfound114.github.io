bce14d53dcd1431c00f222a74f92069e84518fb2
PeterNotFound114 <sicheng2@andrew.cmu.edu>
2024-05-13 16:59:17 -0400

    Info architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import './App.css';
 
-function App() {
-  return (
-    <div className={"App"}>
+export default function App() {
 
-    </div>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/projects',
+      element: <Projects />
+    },
+    {
+      path: '/contact',
+      element: <Contact />
+    }
+  ]);
+
+  return (
+    <RouterProvider router={router} />
   );
 }
-
-export default App;
